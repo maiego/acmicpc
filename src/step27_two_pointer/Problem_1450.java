bd5885564bdd92cfd6c3316a69024e7d85adc7e4
SeungRyong <maiego@naver.com>
2022-08-10 09:35:47 +0900

    투포인터 done

--- src/step27_two_pointer/Problem_1450.java
+++ src/step27_two_pointer/Problem_1450.java
@@ -3,17 +3,55 @@ package step27_two_pointer;
 import java.util.*;
 
 public class Problem_1450 {
+	static int arrN, brrN, k;
+	static int[] arr, brr;
+	
+	static List<Long> sumList = new ArrayList<>();
+	static List<Long> sumList2 = new ArrayList<>();
+	
+	static void f(int idx, long sum, int[] src, List<Long> dst) {
+		if (idx==src.length) {
+			dst.add(sum);
+			return;
+		}
+		f(idx+1, sum, src, dst);
+		f(idx+1, sum+src[idx], src, dst);
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int k = sc.nextInt();
-		int[] arr = new int[n];
-		for (int i=0; i<n; ++i) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
+		k = sc.nextInt();
 
+		int arrN = n-n/2;
+		int[] arr = new int[arrN];
+		for (int i=0; i<arrN; ++i)
+			arr[i] = sc.nextInt();
+
+		int brrN = n-arrN;
+		int[] brr = new int[brrN];
+		for (int i=0; i<brrN; ++i)
+			brr[i] = sc.nextInt();
+		
+		f(0,0, arr, sumList);
+		f(0,0, brr, sumList2);
+		sumList.sort((a,b)->Long.compare(a,b));
+		
+		int ans = 0;
+		for (Long x: sumList2) {
+			if (x>k) continue;
+			int lo=0, hi=sumList.size()-1, cnt=0;
+			while (lo<=hi) {
+				int mid = lo+hi>>1;
+				if (sumList.get(mid)<=k-x) {
+					lo=mid+1;
+					cnt=mid;
+				}
+				else hi=mid-1;
+			}
+			ans+=cnt+1;
+		}
+		System.out.println(ans);
 	}
 
 }
